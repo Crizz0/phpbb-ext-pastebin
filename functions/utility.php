d87c23ba8deef18e99ddf83189e302f1e2169226
Christian Schnegelberger <mail@crizzo.de>
2024-04-03 16:11:01 +0200

    Scan for available languages in highlight src

--- functions/utility.php
+++ functions/utility.php
@@ -1,8 +1,7 @@
 <?php
 /**
 * @package pastebin
-* @version 0.2.0
-* @copyright (c) 2009 3Di (2007 eviL3), 2015 gn#36
+* @copyright (c) 2024 Crizzo
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 */
 
@@ -16,6 +15,9 @@ class utility
 	/* @var \phpbb\language\language */
 	protected $language;
 
+    /* @var */
+    protected $highlight_lang_dir;
+
 	/**
 	 * Constructor
 	 * @param string $php_ext
@@ -23,83 +25,44 @@ class utility
 	 */
 	function __construct(
 		$php_ext,
-		\phpbb\language\language $language)
+		\phpbb\language\language $language,
+        $highlight_lang_dir
+    )
 	{
-		$this->php_ext 		= $php_ext;
-		$this->language		= $language;
-	}
-
-
-	/**
-	 * Check if $needle is in one of geshis supported languages
-	 */
-	/*function geshi_check($needle)
-	{
-		return in_array($needle, $this->geshi_list);
-	}*/
-
-	/**
-	 * List of all geshi langs
-	 */
-	function geshi_list()
-	{
-		$geshi_list = array();
-
-		$d = dir($this->geshi_dir);
-		while (false !== ($file = $d->read()))
-		{
-			if (in_array($file, array('.', '..')))
-			{
-				continue;
-			}
-
-			if (($substr_end = strpos($file, ".$this->php_ext")) !== false)
-			{
-				$geshi_list[] = substr($file, 0, $substr_end);
-			}
-		}
-		$d->close();
-
-		sort($geshi_list);
-
-		return $geshi_list;
+		$this->php_ext 		        = $php_ext;
+		$this->language		        = $language;
+        $this->highlight_lang_dir   = $highlight_lang_dir;
 	}
 
 	/**
-	 * Highlight select box for geshi languages
+	 * List of all available highlight languages
+     * Scan languages dir: \pastebin\vendor\tempest\highlight\src\Languages
+     * Each supported language has an own directory
+	 */
+	function highlight_list_avail_langs()
+	{
+        // Scan the directory of the highlight languages
+       return array_diff(scandir($this->highlight_lang_dir), array('.', '..'));
+       /* TODO filter result via ACP option, to only display activated languages,
+        * in case this list will get really long in the future */
+    }
+
+	/**
+	 * Highlight select box
 	 */
 	function highlight_select($default = 'text')
 	{
-		/** Programming languages used by geshi
-		 * Check \phpbbde\pastebin\vendor\easybook\geshi\geshi for more languages
+		/** Create option menu for available languages
 		 * Add them to the array $programming_langs
-		 * Don't forget to add fitting language variables to \phpbbde\pastebin\language\<iso>\pastebin.php as well
 		 */
-		$programming_langs = array(
-			'base',
-			'blade',
-			'css',
-			'doccomment',
-			'gdscript',
-			'javascript',
-			'json',
-			'php',
-			'sql',
-			'twig',
-			'xml',
-			'yaml',
-		);
-// Make no highlighting as text as default
-		if ($default)
-		{
-
-		}
+        $programming_langs = $this->highlight_list_avail_langs();
 
 		$output = '';
 		$lang_prefix = 'PASTEBIN_LANGS_';
+
 		foreach ($programming_langs as $code)
 		{
-			$output .= '<option' . (($default == $code) ? ' selected="selected"' : '') . ' value="' . htmlentities($code, ENT_QUOTES) . '">' . $this->language->lang($lang_prefix . strtoupper($code)) . '</option>';
+            $output .= '<option' . (($default == $code) ? ' selected ' : '') . ' value="' . htmlentities($code, ENT_QUOTES) . '">' . $this->language->lang($lang_prefix . strtoupper($code)) . '</option>';
         }
 
 		return $output;

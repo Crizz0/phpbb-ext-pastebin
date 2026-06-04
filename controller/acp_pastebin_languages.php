c25ff751e59045a0e56b4f405847be2ca10dfd12
Christian Schnegelberger <mail@crizzo.de>
2026-06-04 18:47:52 +0200

    Use checkboxes for activate and deactivate languages for highlighter

--- controller/acp_pastebin_languages.php
+++ controller/acp_pastebin_languages.php
@@ -21,12 +21,14 @@ class acp_pastebin_languages
 	}
 	public function module_settings(): void
 	{
+		// Get content of pastebin_langs to display the table
 		$sql = 'SELECT lang_id, lang_name, lang_name_clean, lang_active, lang_file_extension FROM '
 			. $this->pastebin_lang_tables .
 			' ORDER BY lang_name_clean ASC';
 
 		$result = $this->db->sql_query($sql);
 
+		// Generate content structure for table
 		while ($rows = $this->db->sql_fetchrow($result))
 		{
 			$this->template->assign_block_vars('langlist', [
@@ -38,11 +40,64 @@ class acp_pastebin_languages
 			]);
 		};
 
+		// Get form input
+		$form_name = 'ACP_PASTEBIN_LANG_SETTINGS';
+		add_form_key($form_name);
+		$error = '';
+
+		if ($this->request->is_set_post('submit'))
+		{
+			if (!check_form_key($form_name))
+			{
+				$error = $this->language->lang('FORM_INVALID');
+			}
+
+			$sql = 'SELECT lang_id FROM ' . $this->pastebin_lang_tables;
+
+			$result = $this->db->sql_query($sql);
+			$rows = $this->db->sql_fetchrowset($result);
+			$rows = array_column($rows, 'lang_id');
+
+			if (empty($error) && $this->request->is_set_post('submit'))
+			{
+				// Grab checked checkboxes
+				$activated_lang = $this->request->variable('lang_active', ['']);
+				// Get not checked checkboxes via array diff
+				$deactivated_lang = array_diff($rows, $activated_lang);
+				// Make a loop for write lang_active = 1 for checked and 0 for not checked
+				foreach ($activated_lang as $activate)
+				{
+					$this->change_lang_settings($activate, 1);
+				}
+				foreach ($deactivated_lang as $deactivate)
+				{
+					$this->change_lang_settings($deactivate, 0);
+				}
+
+				trigger_error($this->language->lang('ACP_PASTEBIN_LANGS_UPDATED') . adm_back_link($this->u_action));
+			}
+		}
+
+		$this->template->assign_vars(array(
+			'U_ACTION' => $this->u_action,
+		));
+
 		$this->db->sql_freeresult($result);
 	}
 
 	public function set_page_url(string $u_action): void
 	{
 		$this->u_action = $u_action;
+	}
+
+	private function change_lang_settings($lang_id, $lang_active): void
+	{
+		$sql = 'UPDATE ' . $this->pastebin_lang_tables . '
+			SET lang_active=' . $lang_active. '
+			WHERE lang_id = ' . (int) $lang_id;
+
+		$result = $this->db->sql_query($sql);
+
+		$this->db->sql_freeresult($result);
 	}
 }
